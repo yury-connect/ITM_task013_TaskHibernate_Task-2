224fa2592c70defcb876531dd984ad618673e669
Yury_Home-PC_Win10_worked <yury.connect@gmail.com>
2024-10-13 16:37:42 +0300

    solution_HIBERNATE: ПОСЛЕ переписыванием слоя DAO через лямбды: СОХРАНЯЮСЬ

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -7,6 +7,8 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,87 +31,54 @@ public class UserDaoHibernateImpl implements UserDao {
                 + "user_lastName VARCHAR(70) NOT NULL, "
                 + "user_age SMALLINT NOT NULL"
                 + ")";
-        executeUpdate(createTableSQL, "Таблица 'users' успешно создана (если не существовала).");
+        executeTransaction(session -> session.createNativeQuery(createTableSQL).executeUpdate());
+        LOGGER.log(Level.FINE, "Таблица 'users' (если она еще не существовала) успешно создана.");
     }
 
 
     @Override
     public void dropUsersTable() {
         String dropTableSQL = "DROP TABLE IF EXISTS users";
-        executeUpdate(dropTableSQL, "Таблица 'users' успешно удалена (если существовала).");
+        executeTransaction(session -> session.createNativeQuery(dropTableSQL).executeUpdate());
+        LOGGER.log(Level.FINE, "Таблица 'users' (если существовала) - то успешно удалена.");
     }
 
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Transaction transaction = null;
-        try (Session session = UtilHibernate.getSessionFactory().openSession();) {
-            transaction = session.beginTransaction(); // Начинаем транзакцию (у Hibernate нужно вручную открывать и закрывать транзауции)
-
-            User userSave = User.builder()
-                    .name(name)
-                    .lastName(lastName)
-                    .age(age)
-                    .build();
-
-            session.save(userSave); // Сохраняем пользователя
-            transaction.commit(); // Подтверждаем транзакцию
-            LOGGER.log(Level.FINE, "Пользователь '" + name + "' успешно сохранен.");
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback(); // Откатываем транзакцию в случае ошибки
-            }
-            LOGGER.log(Level.WARNING, "Ошибка при сохранении пользователя: " + e.getMessage());
-            e.printStackTrace();
-        }
+        User user = User.builder().name(name).lastName(lastName).age(age).build();
+        executeTransaction(session -> session.save(user));
+        LOGGER.log(Level.FINE, "Пользователь '" + name + "' успешно сохранен в DB.");
     }
 
 
     @Override
     public void removeUserById(long id) {
-        Transaction transaction = null;
-        try (Session session = UtilHibernate.getSessionFactory().openSession();) {
-            transaction = session.beginTransaction();
-
-            User userRemove = session.get(User.class, id);
-            if (userRemove != null) {
-                session.remove(userRemove);
+        executeTransaction(session -> {
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.remove(user);
                 LOGGER.log(Level.FINE, "Пользователь с ID " + id + " успешно удален.");
-                System.out.println("Пользователь с ID " + id + " успешно удален.");
             } else {
                 LOGGER.log(Level.WARNING, "Пользователь с ID " + id + " не найден.");
             }
-
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            LOGGER.log(Level.WARNING, "Ошибка при удалении пользователя: " + e.getMessage());
-            e.printStackTrace();
-        }
+        });
     }
 
 
     @Override
     public List<User> getAllUsers() {
-        List<User> users = null;
-        try (Session session = UtilHibernate.getSessionFactory().openSession();) {
-            users = session.createQuery("FROM User", User.class).list();
-
-            LOGGER.log(Level.FINE, "Получено " + users.size() + " пользователей.");
-        } catch (Exception e) {
-            LOGGER.log(Level.WARNING, "Ошибка при получении всех пользователей: " + e.getMessage());
-            e.printStackTrace();
-        }
-        return users;
+        List<User> userList = executeQuery(session -> session.createQuery("FROM User", User.class).list());
+        LOGGER.log(Level.FINE, "Получено " + userList.size() + " пользователей.");
+        return userList;
     }
 
 
     @Override
     public void cleanUsersTable() {
         String cleanTableSQL = "DELETE FROM users";
-        executeUpdate(cleanTableSQL, "Таблица 'users' очищена.");
+        executeTransaction(session -> session.createNativeQuery(cleanTableSQL).executeUpdate());
+        LOGGER.log(Level.FINE, "Таблица 'users' очищена.");
     }
 
 
@@ -117,19 +86,45 @@ public class UserDaoHibernateImpl implements UserDao {
 
 
     // ***** services *****
-    private void executeUpdate(String sql, String successMessage) {
+    private void executeTransaction(Consumer<Session> action) {
         Transaction transaction = null;
         try (Session session = UtilHibernate.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-            session.createNativeQuery(sql).executeUpdate();
+            action.accept(session);
             transaction.commit();
-            LOGGER.log(Level.FINE, successMessage + " : finished");
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            LOGGER.log(Level.WARNING, "Ошибка при выполнении SQL запроса: " + e.getMessage());
+            LOGGER.log(Level.WARNING, "Ошибка при выполнении транзакции: " + e.getMessage());
             e.printStackTrace();
         }
     }
+
+    private <T> T executeQuery(Function<Session, T> query) {
+        T result = null;
+        try (Session session = UtilHibernate.getSessionFactory().openSession()) {
+            result = query.apply(session);
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, "Ошибка при выполнении запроса: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+//    private void executeUpdate(String sql, String successMessage) {
+//        Transaction transaction = null;
+//        try (Session session = UtilHibernate.getSessionFactory().openSession()) {
+//            transaction = session.beginTransaction();
+//            session.createNativeQuery(sql).executeUpdate();
+//            transaction.commit();
+//            LOGGER.log(Level.FINE, successMessage + " : finished");
+//        } catch (Exception e) {
+//            if (transaction != null) {
+//                transaction.rollback();
+//            }
+//            LOGGER.log(Level.WARNING, "Ошибка при выполнении SQL запроса: " + e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
 }
